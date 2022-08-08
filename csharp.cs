fd47b95b78f37d655a12b685efbbe6eb4f14ec06
Kterlecki <49212346+Kterlecki@users.noreply.github.com>
2022-08-08 14:49:25 +0100

    Create csharp.cs

---
+++ csharp.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using System.Text;
+  
+namespace helloWorld
+{
+    class helloWorldPrint
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+   
+        }
+    }
+}
